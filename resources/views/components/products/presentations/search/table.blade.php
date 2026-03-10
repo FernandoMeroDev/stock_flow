9afc0d4e0a1e959d8161b219ebf8868a828b2f0c
Fernando Mero <sd.kettei@gmail.com>
2026-03-09 19:37:17 -0500

    Fixed bugs

--- resources/views/components/products/presentations/search/table.blade.php
+++ resources/views/components/products/presentations/search/table.blade.php
@@ -2,14 +2,16 @@
 
 <x-table class="w-full mb-3">
     @forelse ($presentations as $presentation)
-        <x-table.tr wire:key="{{$presentation->id}}">
-            <td class="p-3 max-w-60 break-words" x-on:click="$dispatch('edit-product', { product_id: {{$presentation->product->id}} })">
-                {{$presentation->complete_name()}}
-            </td>
-            <td class="py-3 flex justify-end">
-                <flux:button icon="plus" x-on:click="$dispatch('add-presentation', { id: {{$presentation->id}} })"/>
-            </td>
-        </x-table.tr>
+        @if($presentation->product)
+            <x-table.tr wire:key="{{$presentation->id}}">
+                <td class="p-3 max-w-60 break-words" x-on:click="$dispatch('edit-product', { product_id: {{$presentation->product->id}} })">
+                    {{$presentation->complete_name()}}
+                </td>
+                <td class="py-3 flex justify-end">
+                    <flux:button icon="plus" x-on:click="$dispatch('add-presentation', { id: {{$presentation->id}} })"/>
+                </td>
+            </x-table.tr>
+        @endif
     @empty
         <x-table.tr>
             <td class="p-3">
