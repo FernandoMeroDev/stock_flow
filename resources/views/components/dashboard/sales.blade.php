ebf08d417074fdbc379e6c4c45753b31a23b8806
Fernando Mero <sd.kettei@gmail.com>
2026-05-12 17:06:31 -0500

    Fixed Bug

--- resources/views/components/dashboard/sales.blade.php
+++ resources/views/components/dashboard/sales.blade.php
@@ -3,10 +3,13 @@
 @use('App\Models\Warehouse')
 
 @if($warehouse = Warehouse::where('name', $warehouseName)->first())
-    <x-dashboard.card :href="route('sales.day', [
-        'warehouse' => $warehouse->id,
-        'day' => date('Y-m-d')
-    ])">
+    <x-dashboard.card
+        :href="route('sales.day', [
+            'warehouse' => $warehouse->id,
+            'day' => date('Y-m-d')
+        ])"
+        permissions="sales"
+    >
         <x-svg.billing class="w-32 h-32" />
         <p class="text-center">Ventas {{$warehouseName}}</p>
     </x-dashboard.card>
